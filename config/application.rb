b83fc644ccc3b4ce4c49309ebfa3a3b220b1b7b9
masahiko kawai <masahikokawai81@gmail.com>
2020-06-16 09:40:12 +0900

    setting rspec
    bin/rails generate rspec:install

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,7 @@ module Rails6
       g.javascripts false
       g.helper false
       g.test_framework :rspec, view_specs: false, helper_specs: false, routing_specs: false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
     end
   end
 end
