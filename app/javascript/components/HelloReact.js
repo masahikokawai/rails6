b5f5c747dc3e46c1fa91c5223ee9547a15ab7317
masahiko kawai <masahikokawai81@gmail.com>
2020-06-16 21:52:04 +0900

    add component

---
+++ app/javascript/components/HelloReact.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+class HelloReact extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        Greeting: {this.props.greeting}
+      </React.Fragment>
+    );
+  }
+}
+
+HelloReact.propTypes = {
+  greeting: PropTypes.string
+};
+export default HelloReact
