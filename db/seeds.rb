73f20a5352d604b14a7b5b740cd554fd601e7f43
masahiko kawai <masahikokawai81@gmail.com>
2020-08-24 19:41:13 +0900

    seed の追加;

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.find_or_create_by!(name: 'todo-1')
+Todo.find_or_create_by!(name: 'todo-2')
+Todo.find_or_create_by!(name: 'todo-3')
+Todo.find_or_create_by!(name: 'todo-4')
+Todo.find_or_create_by!(name: 'todo-5')
