c5b4434dc99a4f2ace64fd93c6f188f13abd7443
masahiko kawai <masahikokawai81@gmail.com>
2020-08-24 20:59:11 +0900

    計算コンポーネントにnameを追加

--- app/javascript/components/Calculation.js
+++ app/javascript/components/Calculation.js
@@ -1,80 +1,29 @@
 import React, { useState } from "react"
 
-// useState
 const Calculation = () => {
-  // const output = useState(0)
-  // console.log({output})
-  // console.log(output)
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const [count, setCount] = useState(0)
-  console.log({count})
-  console.log({setCount})
-  console.log(typeof setCount)
-
-  // const increment = () => {}
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-  const reset = () => setCount(0)
-  const double = () => setCount(count * 2)
-  // const divide3 = () => setCount(previousCount => {
-  //   if (previousCount % 3 === 0) {
-  //     return previousCount / 3
-  //   } else {
-  //     return previousCount
-  //   }
-  // })
-  // const divide3 = () => setCount(previousCount => {
-  //   return previousCount % 3 === 0 ? previousCount / 3 : previousCount
-  // })
-  const divide3 = () => setCount(previousCount => 
-    previousCount % 3 === 0 ? previousCount / 3 : previousCount
-  )
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
 
   return (
-    // 1
-    // <div>
-    //   <div>
-    //     hoge!!<br />
-    //     count: {count}
-    //   </div>
-    //   <button>+1</button>
-    // </div>
-
-    // 2
-    // <React.Fragment>
-    //   <div>
-    //     hoge!!<br />
-    //     count: {count}
-    //   </div>
-    //   <button>+1</button>
-    // </React.Fragment>
-
-    // 3
     <>
-      <div>
-        count: {count}
-      </div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-      </div>
-      <div>
-        <button onClick={double}>×2</button>
-      </div>
-      <div>
-        <button onClick={divide3}>3の倍数の時だけ3で割る</button>
-      </div>
+      <p>現在の{name}は、{price}円です</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      // <button onClick={() => setPrice(props.price)}>Reset</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
     </>
   )
+}
+
+Fuga.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default Calculation
