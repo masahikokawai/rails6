00a2c952967c9a7bf53ad616840e4851342c1e1f
masahiko kawai <masahikokawai81@gmail.com>
2020-08-24 21:04:41 +0900

    use React Hooks useEffect

--- app/javascript/components/Calculation.js
+++ app/javascript/components/Calculation.js
@@ -1,27 +1,47 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
-const Calculation = () => {
-  const [name, setName] = useState(props.name)
-  const [price, setPrice] = useState(props.price)
+// useEffect
+// ライフサイクルの代替
+const Calculation = props => {
+  const [state, setState] = useState(props)
+  const reset = () => setState(props)
+  const {name, price} = state
 
-  const reset = () => {
-    setPrice(props.price)
-    setName(props.name)
+  // jsx のレンダリングの後に呼ばれてる
+  // componentDidMount, componentDidUpdate に似てる
+  // dom初回描画される時、dom都度変更された時
+  useEffect(() => {
+    console.log('useEffect')
+  })
+
+  // ②初回レンダリングに限定したい時
+  useEffect(() => {
+    console.log('useEffect-2')
+  }, [])
+
+  // ③name/price が変更された時に限定したい時
+  // callback
+  useEffect(() => {
+    console.log('useEffect-3')
+  }, [name])
+
+  const renderPeriod = () => {
+    console.log('renderPeriod')
+    return 'maru'
   }
 
   return (
     <>
-      <p>現在の{name}は、{price}円です</p>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      // <button onClick={() => setPrice(props.price)}>Reset</button>
+      <p>現在の{name}は、{price}円です3{renderPeriod()}</p>
+      <button onClick={() => setState({...state, price: price + 1})}>+1</button>
+      <button onClick={() => setState({...state, price: price - 1})}>-1</button>
       <button onClick={reset}>Reset</button>
-      <input value={name} onChange={e => setName(e.target.value)}/>
+      <input value={state.name} onChange={e => setState({...state, name: e.target.value})}/>
     </>
   )
 }
 
-Fuga.defaultProps = {
+Calculation.defaultProps = {
   name: '',
   price: 1000
 }
